dfc3064b1c02d5ecf0387c47bd7750bfe37de80d
Peter Ellis <Peter.Ellis@nousgroup.com.au>
2020-09-25 16:28:26 +1000

    both fixed and free vertical scales

--- explore/explore-full-data.R
+++ explore/explore-full-data.R
@@ -23,8 +23,7 @@ raw_data %>%
 # The most responses are from Brazil, Mexico, Japan:
 raw_data %>%
   filter(age_bucket == "overall" & gender == "overall") %>%
-  count(country_agg, wt = total_responses, sort = TRUE) %>%
-  View
+  count(country_agg, wt = total_responses, sort = TRUE) 
 # Australia is 27 with 266,000 responses,  UK 12 with 400,000, USA is excluded?
 
 raw_data %>%
@@ -34,19 +33,30 @@ raw_data %>%
 CairoPDF("output/lots-of-vars-6-countries.pdf", 11, 8)
 for(j in 9:ncol(raw_data)){
   raw_data$tmp <- pull(raw_data, j)
-  p <- raw_data %>%
+  p0 <- raw_data %>%
     filter(country_agg %in% c("Australia", "Brazil", "United Kingdom", 
                               "Italy","New Zealand", "Iraq")) %>%
     filter(age_bucket == "overall") %>%
     filter(gender %in% c("female", "male")) %>%
     ggplot(aes(x = date, y = tmp, colour = gender)) +
-    facet_wrap(~country_agg, scales = "free_y") +
     geom_point(size = 0.3) +
     geom_smooth(se = FALSE, span = 1/7, method = "loess", formula = "y ~ x") +
     labs(y = names(raw_data)[j],
          title = names(raw_data)[j],
          x = "Date in 2020")
-  print(p)
+  
+  p1 <- p0 + 
+    facet_wrap(~country_agg, scales = "free_y") +
+    labs(subtitle = "Free vertical scales")
+  
+  p2 <- p0 + 
+    facet_wrap(~country_agg, scales = "fixed") +
+    labs(subtitle = "fixed vertical scales")
+  
+  print(p1)
+  print(p2)
+  
+  raw_data$tmp <- NULL
 }
 dev.off()
 
